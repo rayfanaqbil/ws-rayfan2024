f73fd116f71d6ad50ed024392270f1bdeaff8b06
rayfanaqbil <rayfana09@gmail.com>
2024-06-07 23:30:09 +0700

    push delete

--- url/url.go
+++ url/url.go
@@ -22,5 +22,6 @@ func Web(page *fiber.App) {
 	page.Get("/presensi/:id", controller.GetPresensiID)
 	page.Post("/insert", controller.InsertDataPresensi)
 	page.Put("/update/:id", controller.UpdateData)
+	page.Delete("/delete/:id", controller.DeletePresensiByID)
 
 }
